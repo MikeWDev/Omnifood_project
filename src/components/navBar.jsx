9d0b0f7e406d0d5f01a81611211d21bdd08c3ea1
MikeWDev <130941015+MikeWDev@users.noreply.github.com>
2023-11-16 20:29:09 +0000

    Fixed styles bugs for card section

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,8 +1,44 @@
 import { useEffect, useState } from "react";
 import { OmniFoodLogo } from "../assets/img/imgIndex";
 import { Link } from "react-scroll";
+
+
 const NavBar = () => {
+  const[IntersectinnState, setIntersectingState] = useState(true);
+
+useEffect(() =>{
+const obs = new IntersectionObserver(
+  function (entires){
+    const ent = entires[0];
+    const bodyEl = document.body;
   
+    if(ent.isIntersecting===false){
+      bodyEl.classList.add("sticky");
+      setIntersectingState(false)
+      console.log(bodyEl);
+    }else{
+      if(ent.isIntersecting===true){
+        bodyEl.classList.remove("sticky");
+        setIntersectingState(true)
+        console.log(bodyEl);
+      }
+    }
+  },
+  {
+    root:null,
+    threshold:0,
+    rootMargin:"-90px"
+  }
+);
+const heroEl = document.querySelectorAll(".section-hero");
+
+heroEl.forEach((el) =>{
+  obs.observe(el)
+})
+}, [IntersectinnState])
+
+
+
 
  
   
--- src/hooks/useSticky.jsx
+++ src/hooks/useSticky.jsx
@@ -1,33 +1,37 @@
-const[IntersectinnState, setIntersectingState] = useState(true);
-const[stickyClass, setStickyClass] = useState("");
-useEffect(() =>{
-const obs = new IntersectionObserver(
-  function (entires){
-    const ent = entires[0];
-    const bodyEl = document.body;
+// function useSticky()  {
+// const[IntersectinnState, setIntersectingState] = useState(true);
+// const[stickyClass, setStickyClass] = useState("");
+// useEffect(() =>{
+// const obs = new IntersectionObserver(
+//   function (entires){
+//     const ent = entires[0];
+//     const bodyEl = document.body;
   
-    if(ent.isIntersecting===false){
-      bodyEl.classList.add("sticky");
-      setIntersectingState(false)
-      console.log(bodyEl);
-    }else{
-      if(ent.isIntersecting===true){
-        bodyEl.classList.remove("sticky");
-        setIntersectingState(true)
-        console.log(bodyEl);
-      }
-    }
-  },
-  {
-    root:null,
-    threshold:0,
-    rootMargin:"-90px"
-  }
-);
-const heroEl = document.querySelectorAll(".section-hero");
+//     if(ent.isIntersecting===false){
+//       bodyEl.classList.add("sticky");
+//       setIntersectingState(false)
+//       console.log(bodyEl);
+//     }else{
+//       if(ent.isIntersecting===true){
+//         bodyEl.classList.remove("sticky");
+//         setIntersectingState(true)
+//         console.log(bodyEl);
+//       }
+//     }
+//   },
+//   {
+//     root:null,
+//     threshold:0,
+//     rootMargin:"-90px"
+//   }
+// );
+// const heroEl = document.querySelectorAll(".section-hero");
 
-heroEl.forEach((el) =>{
-  obs.observe(el)
-})
-}, [IntersectinnState])
+// heroEl.forEach((el) =>{
+//   obs.observe(el)
+// })
+// }, [IntersectinnState])
 
+// }
+
+// export default useSticky;
